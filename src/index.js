61300d37b250d82fd45e4c903473e42ec659412a
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-05-15 15:05:00 +0300

    completed gift

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 import burgMenu from './modules/burgMenu';
 import chooseClub from './modules/chooseClub';
 import headSlider from './modules/headSlider';
+import gift from './modules/gift';
 
 
 burgMenu();
 chooseClub();
 headSlider();
+gift();
