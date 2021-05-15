b240b86ab9fdf1b7cb3738ed95e847e3f6d1873d
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-05-15 14:40:00 +0300

    completed head slider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 
 import burgMenu from './modules/burgMenu';
 import chooseClub from './modules/chooseClub';
+import headSlider from './modules/headSlider';
 
 
 burgMenu();
 chooseClub();
+headSlider();
