23c650d6a087a785048b0c504e9b0fd26de0147b
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-05-15 16:26:47 +0300

    completed arrow

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import burgMenu from './modules/burgMenu';
 import chooseClub from './modules/chooseClub';
 import headSlider from './modules/headSlider';
 import gift from './modules/gift';
+import arrow from './modules/arrow';
 
 
 burgMenu();
 chooseClub();
 headSlider();
 gift();
+arrow();
